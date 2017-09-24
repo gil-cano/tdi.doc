c67fc1c6a160eb7863e689ee2dafd897e38a5d61
gil-cano <gil.cano@gmail.com>
2017-09-23 23:53:15 -0500

    exclude lib

--- conf.py
+++ conf.py
@@ -73,7 +73,7 @@ release = '1.0a'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'lib', 'README.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
